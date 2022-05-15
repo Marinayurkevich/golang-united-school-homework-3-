d043fc8571989f3de6d0d59f6841bfe7aa56bccf
marina <roxy-lo@inbox.ru>
2022-05-15 15:14:31 +0300

    2nd att

--- task01-arrays.go
+++ task01-arrays.go
@@ -2,8 +2,8 @@ package homework
 
 func average(input [15]float32) (result float32) {
 	var sum float32 = 0
-	for _, array := range array{
-	sum+=array}
-	N:=float32 (len(array))	
+	for _, input := range input{
+	sum+=input}
+	N:=float32 (len(input))	
 	return sum/N
 }
