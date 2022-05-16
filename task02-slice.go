1a2e8f480b57ad693d87646a4cd144af2d616dce
marina <roxy-lo@inbox.ru>
2022-05-16 16:12:34 +0300

    slice2

--- task02-slice.go
+++ task02-slice.go
@@ -1,8 +1,8 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	result := make([]int,len(input))
+	result:= make([]int,len(input))
 	for i:=len(input)-1;i>=0;i-- {
-	result[i] = i
-	return
+	result[i] = i}
+	return result
 }
